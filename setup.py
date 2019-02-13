5c51e8b96fe9f6b2d98ed798e0df3bcc7be1513a
Rob Moore <giftiger.wunsch@xantoria.com>
2019-02-13 23:29:06 +0000

    Make setup pass pypi standards

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
+#!/usr/bin/env python3
+import os
 from setuptools import setup
+
+
+def readme_content():
+    dir = os.path.abspath(os.path.dirname(__file__))
+    desc = None
+    with open(os.path.join(dir, 'README.md'), 'r') as f:
+        desc = f.read()
+
+    return desc
 
 setup(
     name='s3_browser',
@@ -9,5 +20,7 @@ setup(
             's3-browser=s3_browser.cli:main'
         ]
     },
-    install_requires=['boto3>=1.9.0']
+    install_requires=['boto3>=1.9.0'],
+    long_description=readme_content(),
+    long_description_content_type='text/markdown'
 )
