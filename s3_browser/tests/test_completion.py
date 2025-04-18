c9e39e9bfe4d8067906b25c0ac0c58feb00d54ed
Rob Moore <giftiger.wunsch@gmail.com>
2025-04-18 16:08:45 +0100

    Fix readline mock

--- s3_browser/tests/test_completion.py
+++ s3_browser/tests/test_completion.py
@@ -14,17 +14,17 @@ class TestCompletion:
         m.RECOGNISED_COMMANDS = Cli.RECOGNISED_COMMANDS
         return CliCompleter(m)
 
-    def _complete(self, completer, get_line_buffer, text, state):
+    def _complete(self, completer, readline, text, state):
         """
         Make the readline line buffer mock return the value we want, and then
         check completer.complete with that in mind.
         """
-        get_line_buffer.return_value = text
+        readline.get_line_buffer.return_value = text
         res = completer.complete(None, state)
-        get_line_buffer.reset_mock()
+        readline.reset_mock()
         return res
 
-    @patch("gnureadline.get_line_buffer")
+    @patch("s3_browser.completion.readline")
     def test_complete_empty_command(self, mock):
         """Tab on an empty string should list all commands"""
         completer = self._completer()
@@ -32,7 +32,7 @@ class TestCompletion:
         for i, cmd in enumerate(Cli.RECOGNISED_COMMANDS):
             assert self._complete(completer, mock, "", i) == cmd
 
-    @patch("gnureadline.get_line_buffer")
+    @patch("s3_browser.completion.readline")
     def test_complete_partial_command(self, mock):
         completer = self._completer()
         assert self._complete(completer, mock, "c", 0) == "cat"
@@ -40,7 +40,7 @@ class TestCompletion:
         assert self._complete(completer, mock, "c", 2) == "clear"
         assert self._complete(completer, mock, "bo", 0) == "bookmark"
 
-    @patch("gnureadline.get_line_buffer")
+    @patch("s3_browser.completion.readline")
     def test_complete_s3_path_commands(self, mock):
         """Tab on several commands should complete S3 paths or keys"""
         completer = self._completer()
@@ -76,7 +76,7 @@ class TestCompletion:
         for i, f in enumerate(["../", *expected_files]):
             assert self._complete(completer, mock, "cat ..", i) == f
 
-    @patch("gnureadline.get_line_buffer")
+    @patch("s3_browser.completion.readline")
     def test_complete_local_path(self, mock):
         """Tab on put should complete s3 path or local path arguments"""
         completer = self._completer()
@@ -86,7 +86,7 @@ class TestCompletion:
             assert self._complete(completer, mock, "put ", i) == f
             assert self._complete(completer, mock, "get . ", i) == f
 
-    @patch("gnureadline.get_line_buffer")
+    @patch("s3_browser.completion.readline")
     @patch("os.path.expanduser")
     def test_complete_local_path_tilde(self, mock_expanduser, mock_readline):
         """Should replace a lone ~ with the home dir path"""
@@ -99,7 +99,7 @@ class TestCompletion:
         assert self._complete(completer, mock_readline, "put ~", 1) is None
         assert self._complete(completer, mock_readline, "get . ~", 1) is None
 
-    @patch("gnureadline.get_line_buffer")
+    @patch("s3_browser.completion.readline")
     @patch("os.path.expanduser")
     def test_complete_local_path_tilde_path(self, mock_expanduser, mock_readline):
         """Should complete paths containing ~ as home dir"""
@@ -113,7 +113,7 @@ class TestCompletion:
             assert self._complete(completer, mock_readline, "get . ~/", i) == f
             mock_expanduser.assert_has_calls([call("~/"), call("~/")])
 
-    @patch("gnureadline.get_line_buffer")
+    @patch("s3_browser.completion.readline")
     def test_complete_paths_with_quotes(self, mock):
         """Tab complete should work where paths need quoting"""
         completer = self._completer()
